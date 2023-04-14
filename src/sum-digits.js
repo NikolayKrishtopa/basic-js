91e7fcc7d63ba3fe044ed4f5650e0ff343bae36d
Nikolay Krishtopa <nikolay.krishtopa@gmail.com>
2023-04-15 02:44:29 +0400

    feat: sort by height

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,11 +12,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let res = String(n)
+    .split('')
+    .map((e) => Number(e))
+    .reduce((a, b) => a + b);
+  return String(res).length === 1 ? res : getSumOfDigits(res);
 }
 
 module.exports = {
-  getSumOfDigits
+  getSumOfDigits,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -11,15 +11,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-function getSeason(date) {
-  const month = date.getMonth();
-  return month < 2
-    ? 'winter'
-    : month < 5
-    ? 'spring'
-    : month < 8
-    ? 'autumn'
-    : 'winter';
+function getSeason(/* date */) {
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
 }
 
 module.exports = {
